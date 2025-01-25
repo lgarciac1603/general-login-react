9ce0c8d242d53eaf71e0cc1ac5d42045aaf32284
lgarciac1603 <lfgc851@gmail.com>
2025-01-24 22:24:38 -0600

    Login first logic adjust

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useSelector } from "react-redux";
+import { RootState } from "./context/store";
+import { Login } from "./pages/Login";
+import { Dashboard } from "./pages/Dashboard";
+
 import "./styles/_global.scss";
 
 function App() {
-  return <>General Login</>;
+  const userId = useSelector((state: RootState) => state.session.id);
+
+  // TODO: be more strict with session verification
+  return userId && userId.length > 1 ? <Dashboard /> : <Login />;
 }
 
 export default App;
